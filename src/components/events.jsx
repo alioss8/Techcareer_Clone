e63b6e5ac3d69abfa215859ef4f38159f02b260b
Ali Osman Ilık <aliosmanilik8@gmail.com>
2025-03-06 13:05:42 +0300

    feat/event.css/styling-elements

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import  {Col , Stack,Row,Container,Button}   from 'react-bootstrap';
-
+import '../styles/events.css'
 
 function Events() {
 return (
 <div>
 <Container>
-     
+
     <Row className="justify-content-center">
-        <Col xs={11} className="bg-light p-3 border m-5">
+        <Col xs={11} className="bg-light p-3 m-5">
         <h1>Etkinliklerimiz</h1>
         <Stack  direction="horizontal" className="justify-content-center">
-        <Button variant="light">Bootcamp</Button>
-        <Button variant="light">Hackathon</Button>
-        <Button variant="light">Hiring Challange</Button>
+        <Button className='eventsbtn col-4' id="bootcampbtn" variant="light">Bootcamp</Button>
+        <Button className='eventsbtn col-4' variant="light">Hackathon</Button>
+        <Button className='eventsbtn col-4'   variant="light">Hiring Challange</Button>
         </Stack>
         <Stack>
             <Container>
-                <p>Uzman eğitmenler ile farklı yazılım dillerinde eğitimler alıp, projeler üzerinde çalışarak gerçek dünya tecrübesi edin, kariyerinde hızla yüksel.</p>
-            </Container>,
-            <Button variant="link">Hemen Başvur</Button>
+                <p className='eventsparag' >Uzman eğitmenler ile farklı yazılım dillerinde eğitimler alıp, projeler üzerinde <br /> çalışarak gerçek dünya tecrübesi edin, kariyerinde hızla yüksel.</p>
+            </Container>
+            <Button className='aplicationbtn'   variant="link">Hemen Başvur <i class="bi bi-arrow-right"></i>  </Button>
         </Stack>
 
         </Col>
