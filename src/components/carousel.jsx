c5eea7b7f777cf1e98a4c1627bef018cf21ebbfd
Ali Osman Ilık <aliosmanilik8@gmail.com>
2025-03-10 13:07:09 +0300

    fix/carousel.jsx-Carousel.css/fixxing-size-and-responsive

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -8,35 +8,73 @@ const CenterModeSlider = () => {
     dots: true, // Alt kısımda noktalar göster
     infinite: true, // Sonsuz döngü
     speed: 300, // Geçiş hızı
-    slidesToShow: 3, // Aynı anda gösterilecek slayt sayısı
+    slidesToShow: 1, // Aynı anda gösterilecek slayt sayısı
     slidesToScroll: 1, // Kaydırma sırasında geçilecek slayt sayısı
     centerMode: true, // Ortalama modunu aktif et
-    centerPadding: '0', // Ortadaki slaytın yanlarındaki boşluk
+    centerPadding: '200px', // Ortadaki slaytın yanlarındaki boşluk
     focusOnSelect: true, // Slayta tıklandığında odaklan
-    autoplay: true, // Otomatik kaydırma
-    autoplaySpeed: 3000, // Otomatik kaydırma hızı (ms cinsinden)
+    autoplay: false, // Otomatik kaydırma
+    autoplaySpeed: 5000, 
+    responsive: [
+      {
+        breakpoint: 1024, // 1024px altına düşünce
+        settings: {
+          slidesToShow: 2, // 2 resim göster
+        },
+      },
+      {
+        breakpoint: 768, // 768px altına düşünce
+        settings: {
+          slidesToShow: 1, // 1 resim göster
+        },
+      },
+    ],
+    // Otomatik kaydırma hızı (ms cinsinden)
   };
 
   return (
-    <div style={{ width: '80%', margin: 'auto' }}>
+   <div style={{ width: '100%', margin: 'auto',  background:"#293349", marginTop:"100px" }} >
+    <div style={{ width: '100%', margin: 'auto', paddingTop:"60px", paddingBottom:"50px" }}>
       
       <Slider {...settings}>
         <div>
-          <img src="/src/assets/Carousel.webp" alt="Slide 1" />
+          <a href="#"></a>
+          <img src="/src/assets/Carousel_webp/Carousel.webp" alt="Slide 1" />
         </div>
         <div>
-          <img src="/src/assets/Carousel.webp" alt="Slide 2" />
+        <a href="#"></a>
+          <img src="/src/assets/Carousel_webp/Gs.webp" alt="Slide 2" />
         </div>
         <div>
-          <img src="/src/assets/Carousel.webp" alt="Slide 3" />
+        <a href="#"></a>
+          <img src="/src/assets/Carousel_webp/is_ilanlari_banner.webp" alt="Slide 3" />
         </div>
         <div>
-          <img src="/src/assets/Carousel.webp" alt="Slide 4" />
+        <a href="#"></a>
+          <img src="/src/assets/Carousel_webp/Senior_Komuenite_Duyuru_.webp" alt="Slide 4" />
         </div>
         <div>
-          <img src="/src/assets/Carousel.webp" alt="Slide 5" />
+        <a href="#"></a>
+          <img src="/src/assets/Carousel_webp/senior_yazilimci_banner_.webp" alt="Slide 5" />
+        </div>
+        <div>
+        <a href="#"></a>
+          <img src="/src/assets/Carousel_webp/slack_banner_.webp" alt="Slide 1" />
+        </div>
+        <div>
+        <a href="#"></a>
+          <img src="/src/assets/Carousel_webp/sozluk_banner_.webp" alt="Slide 1" />
+        </div>
+        <div>
+        <a href="#"></a>
+          <img src="/src/assets/Carousel_webp/testler_banner_.webp" alt="Slide 1" />
+        </div>
+        <div>
+        <a href="#"></a>
+          <img src="/src/assets/Carousel_webp/video_egitimler_banner_.webp" alt="Slide 1" />
         </div>
       </Slider>
+    </div>
     </div>
   );
 };
